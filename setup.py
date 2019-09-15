7e8e8ec6f10c8c7d29683846165010406f79221b
Mike Helmick <mikehelmick@me.com>
2019-09-15 13:38:55 -0400

    Try setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="isort",
+    version='0.3.0',
+    description="isort hook.",
+    py_modules=["isort"],
+    python_requires=">=3.7",
+    install_requires=[
+        "isort",
+    ],
+)
